negative recovery rate fix, 5 year trade factor average for scaling

src/base_model/tramp_econ_model.py
@@ -306,10 +306,13 @@ def calc_tramp_econ_model_over_trs(Q_St, Q_Fabrication_Buffer, Q_EoL, S_EoL_net_
     R_recov_p = R_recov_g(2, P_col)
     R_recov_t = R_recov_g(3, P_col)
 
-    r_recov_g = np.array([R_recov_c, R_recov_m, R_recov_p, R_recov_t])
-
     if R_recov_p < 0:
         R_recov_p = 0
+
+    r_recov_g = np.array([R_recov_c, R_recov_m, R_recov_p, R_recov_t])
+
+    if np.any(r_recov_g < 0):
+        raise RuntimeError('Recovery rate should not be negative.')
 
     P_dis_col_eaf = P_col + P_dis + exog_EAF
     price_diff = P_PrSt - P_dis_col_eaf

src/calc_trade/calc_scrap_trade.py
@@ -17,9 +17,11 @@ def get_scrap_trade(country_specific, available_scrap_by_category):
 
     scrap_imports, scrap_exports = _recalculate_scrap_trade_based_on_scrap_availability(net_scrap_trade,
                                                                                         available_scrap_by_category)
+
     scrap_imports, scrap_exports = _split_scrap_trade_into_waste_categories(scrap_imports,
                                                                             scrap_exports,
                                                                             available_scrap_by_category)
+
     # We assume no scrap trade in the first year due to buffer
     scrap_imports[0] = 0
     scrap_exports[0] = 0
@@ -63,10 +65,6 @@ def _recalculate_scrap_trade_based_on_scrap_availability(projected_scrap_trade, 
     scrap_exports = np.minimum(scrap_exports, available_scrap)
 
     if np.any(factor[124:] < 0):
-        a = 0
-
-    test = np.all((available_scrap - scrap_exports)[124:] > 0)
-    if not test:
         a = 0
 
     return scrap_imports, scrap_exports